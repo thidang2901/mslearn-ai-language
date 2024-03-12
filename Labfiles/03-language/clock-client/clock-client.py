7e3ac9d9c7d3d3e378e8b82155150f19d16e2cee
Thi Dang <dkthi2901@gmail.com>
2024-03-13 00:40:24 +0700

    add code lab 03

--- Labfiles/03-language/clock-client/clock-client.py
+++ Labfiles/03-language/clock-client/clock-client.py
@@ -1,19 +1,23 @@
-from dotenv import load_dotenv
 import os
-import json
 from datetime import datetime, timedelta, date
-from dateutil.parser import parse as is_date
+from dotenv import load_dotenv
 
 # Import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.conversations import ConversationAnalysisClient
 
 
 def main():
-
     try:
         # Get Configuration Settings
         load_dotenv()
         ls_prediction_endpoint = os.getenv('LS_CONVERSATIONS_ENDPOINT')
         ls_prediction_key = os.getenv('LS_CONVERSATIONS_KEY')
+        cls_project = os.getenv('LS_PROJECT_NAME')
+        deployment_slot = os.getenv('LS_DEPLOYMENT_NAME')
+
+        # Create a client for the Language service model
+        client = ConversationAnalysisClient(ls_prediction_endpoint, AzureKeyCredential(ls_prediction_key))
 
         # Get user input (until they enter "quit")
         userText = ''
@@ -21,11 +25,87 @@ def main():
             userText = input('\nEnter some text ("quit" to stop)\n')
             if userText.lower() != 'quit':
 
-                # Create a client for the Language service model
-
                 # Call the Language service model to get intent and entities
+                with client:
+                    query = userText
+                    result = client.analyze_conversation(
+                        task={
+                            "kind": "Conversation",
+                            "analysisInput": {
+                                "conversationItem": {
+                                    "participantId": "1",
+                                    "id": "1",
+                                    "modality": "text",
+                                    "language": "en",
+                                    "text": query
+                                },
+                                "isLoggingEnabled": False
+                            },
+                            "parameters": {
+                                "projectName": cls_project,
+                                "deploymentName": deployment_slot,
+                                "verbose": True
+                            }
+                        }
+                    )
+
+                top_intent = result["result"]["prediction"]["topIntent"]
+                entities = result["result"]["prediction"]["entities"]
+
+                print("view top intent:")
+                print("\ttop intent: {}".format(result["result"]["prediction"]["topIntent"]))
+                print("\tcategory: {}".format(result["result"]["prediction"]["intents"][0]["category"]))
+                print(
+                    "\tconfidence score: {}\n".format(result["result"]["prediction"]["intents"][0]["confidenceScore"]))
+
+                print("view entities:")
+                for entity in entities:
+                    print("\tcategory: {}".format(entity["category"]))
+                    print("\ttext: {}".format(entity["text"]))
+                    print("\tconfidence score: {}".format(entity["confidenceScore"]))
+
+                print("query: {}".format(result["result"]["query"]))
 
                 # Apply the appropriate action
+                if top_intent == 'GetTime':
+                    location = 'local'
+                    # Check for entities
+                    if len(entities) > 0:
+                        # Check for a location entity
+                        for entity in entities:
+                            if 'Location' == entity["category"]:
+                                # ML entities are strings, get the first one
+                                location = entity["text"]
+                    # Get the time for the specified location
+                    print(GetTime(location))
+
+                elif top_intent == 'GetDay':
+                    date_string = date.today().strftime("%m/%d/%Y")
+                    # Check for entities
+                    if len(entities) > 0:
+                        # Check for a Date entity
+                        for entity in entities:
+                            if 'Date' == entity["category"]:
+                                # Regex entities are strings, get the first one
+                                date_string = entity["text"]
+                    # Get the day for the specified date
+                    print(GetDay(date_string))
+
+                elif top_intent == 'GetDate':
+                    day = 'today'
+                    # Check for entities
+                    if len(entities) > 0:
+                        # Check for a Weekday entity
+                        for entity in entities:
+                            if 'Weekday' == entity["category"]:
+                                # List entities are lists
+                                day = entity["text"]
+                    # Get the date for the specified day
+                    print(GetDate(day))
+
+                else:
+                    # Some other intent (for example, "None") was predicted
+                    print('Try asking me for the time, the day, or the date.')
 
     except Exception as ex:
         print(ex)
@@ -41,41 +121,42 @@ def GetTime(location):
 
     if location.lower() == 'local':
         now = datetime.now()
-        time_string = '{}:{:02d}'.format(now.hour,now.minute)
+        time_string = '{}:{:02d}'.format(now.hour, now.minute)
     elif location.lower() == 'london':
         utc = datetime.utcnow()
-        time_string = '{}:{:02d}'.format(utc.hour,utc.minute)
+        time_string = '{}:{:02d}'.format(utc.hour, utc.minute)
     elif location.lower() == 'sydney':
         time = datetime.utcnow() + timedelta(hours=11)
-        time_string = '{}:{:02d}'.format(time.hour,time.minute)
+        time_string = '{}:{:02d}'.format(time.hour, time.minute)
     elif location.lower() == 'new york':
         time = datetime.utcnow() + timedelta(hours=-5)
-        time_string = '{}:{:02d}'.format(time.hour,time.minute)
+        time_string = '{}:{:02d}'.format(time.hour, time.minute)
     elif location.lower() == 'nairobi':
         time = datetime.utcnow() + timedelta(hours=3)
-        time_string = '{}:{:02d}'.format(time.hour,time.minute)
+        time_string = '{}:{:02d}'.format(time.hour, time.minute)
     elif location.lower() == 'tokyo':
         time = datetime.utcnow() + timedelta(hours=9)
-        time_string = '{}:{:02d}'.format(time.hour,time.minute)
+        time_string = '{}:{:02d}'.format(time.hour, time.minute)
     elif location.lower() == 'delhi':
         time = datetime.utcnow() + timedelta(hours=5.5)
-        time_string = '{}:{:02d}'.format(time.hour,time.minute)
+        time_string = '{}:{:02d}'.format(time.hour, time.minute)
     else:
         time_string = "I don't know what time it is in {}".format(location)
-    
+
     return time_string
+
 
 def GetDate(day):
     date_string = 'I can only determine dates for today or named days of the week.'
 
     weekdays = {
-        "monday":0,
-        "tuesday":1,
-        "wednesday":2,
-        "thursday":3,
-        "friday":4,
-        "saturday":5,
-        "sunday":6
+        "monday": 0,
+        "tuesday": 1,
+        "wednesday": 2,
+        "thursday": 3,
+        "friday": 4,
+        "saturday": 5,
+        "sunday": 6
     }
 
     today = date.today()
@@ -92,6 +173,7 @@ def GetDate(day):
 
     return date_string
 
+
 def GetDay(date_string):
     # Note: To keep things simple, dates must be entered in US format (MM/DD/YYYY)
     try:
@@ -100,6 +182,7 @@ def GetDay(date_string):
     except:
         day_string = 'Enter a date in MM/DD/YYYY format.'
     return day_string
+
 
 if __name__ == "__main__":
     main()
