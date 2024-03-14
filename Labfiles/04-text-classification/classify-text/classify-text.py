960cd9e9c7f7d41a17b5ce8b3ee5931eede557fd
Thi Dang <dkthi2901@gmail.com>
2024-03-15 01:39:33 +0700

    add code lab 04

--- Labfiles/04-text-classification/classify-text/classify-text.py
+++ Labfiles/04-text-classification/classify-text/classify-text.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -10,11 +11,12 @@ def main():
         load_dotenv()
         ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
         ai_key = os.getenv('AI_SERVICE_KEY')
-        project_name = os.getenv('CLASSIFY_PROJECT')
-        deployment_name = os.getenv('CLASSIFY_DEPLOYMENT')
+        project_name = os.getenv('CLASSIFY_PROJECT_NAME')
+        deployment_name = os.getenv('CLASSIFY_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
         # Read each text file in the articles folder
         batchedDocuments = []
@@ -26,7 +28,24 @@ def main():
             batchedDocuments.append(text)
 
         # Get Classifications
+        operation = ai_client.begin_single_label_classify(
+            batchedDocuments,
+            project_name=project_name,
+            deployment_name=deployment_name
+        )
 
+        document_results = operation.result()
+
+        for doc, classification_result in zip(files, document_results):
+            if classification_result.kind == "CustomDocumentClassification":
+                classification = classification_result.classifications[0]
+                print("{} was classified as '{}' with confidence score {}.".format(
+                    doc, classification.category, classification.confidence_score)
+                )
+            elif classification_result.is_error is True:
+                print("{} has an error with code '{}' and message '{}'".format(
+                    doc, classification_result.error.code, classification_result.error.message)
+                )
 
 
     except Exception as ex:
